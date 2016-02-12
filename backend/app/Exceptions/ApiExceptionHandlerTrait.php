0d26a06d8d269a42f05413669d2256a4bd71af08
mikakane <mikakane2@gmail.com>
2016-02-12 21:02:09 +0900

    update

---
+++ backend/app/Exceptions/ApiExceptionHandlerTrait.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkkn
+ * Date: 2016/01/31
+ * Time: 19:08
+ */
+
+namespace App\Exceptions;
+
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\ResponseTrait;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+use Exception;
+trait ApiExceptionHandlerTrait
+{
+    protected function handleError(Exception $e,JsonResponse $res){
+        if($e instanceof HttpException){
+            $res->setStatusCode(400);
+            $res->setData([
+                "status" => "BAD",
+                "msg" => "not found"
+            ]);
+            return $res;
+        }else{
+            return $this->getErrorResponse($e,$res);
+        }
+    }
+
+
+    protected function getErrorResponse(Exception $e,JsonResponse $res){
+        $data = $this->formatException($e);
+        $data["status"] = "ERROR";
+
+        $res->setData($data);
+        $res->setStatusCode(500);
+        return $res;
+    }
+
+    protected function formatException(Exception $e){
+        if(env("APP_ENV") === "local"){
+            return $this->formatExceptionOnLocal($e);
+        }else{
+            return $this->formatExceptionOnProduction($e);
+        }
+    }
+
+    protected function formatExceptionOnLocal(Exception $e){
+        return [
+            "msg" => $e->getMessage(),
+            "line" => $e->getLine(),
+            "file" => $e->getFile(),
+            "code" => $e->getCode(),
+            "previous" => $e->getPrevious(),
+            "trace" => $e->getTrace(),
+            "class" => get_class($e),
+        ];
+    }
+
+    protected function formatExceptionOnProduction(Exception $e){
+        return [
+            "msg" => "whoops, something wrong"
+        ];
+    }
+
+}
---
+++ backend/app/Exceptions/Handler.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Exceptions;
+
+use Chatbox\Begonia\Http\Response\JsonResponseTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\ResponseTrait;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    use JsonResponseTrait;
+
+    use ApiExceptionHandlerTrait;
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $e
+     * @return void
+     */
+    public function report(Exception $e)
+    {
+        parent::report($e);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $e
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $e)
+    {
+        $res = $this->getResponse([],500);
+        return $this->handleError($e,$res);
+    }
+}
